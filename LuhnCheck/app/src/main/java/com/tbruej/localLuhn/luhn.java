914d9adb4631c2ebda68f30b2c508042439317d3
MohammedJamal <mohammed.jamalhadi@gmail.com>
2019-04-16 11:05:40 +0200

    Update : Implementation of luhn algorithm local

---
+++ LuhnCheck/app/src/main/java/com/tbruej/localLuhn/luhn.java
@@ -0,0 +1,35 @@
+package com.tbruej.localLuhn;
+
+public class luhn {
+
+    public static boolean checkLuhn(String cardNo){
+        int nDigit = cardNo.length();
+        int nSum = 0;
+        boolean isSecond = false;
+
+        for(int i = nDigit - 1 ; i >= 0; i--){
+
+            int d = cardNo.charAt(i) - '0';
+
+            if (isSecond == true)
+                d = d * 2;
+
+            nSum += d  / 10;
+            nSum += d % 10;
+
+            isSecond = !isSecond;
+        }
+
+        return (nSum % 10 == 0);
+    }
+
+
+    public static void main(String[] args) {
+
+        String cardNo = "18009054331492";
+        if (checkLuhn(cardNo))
+            System.out.println("VALID!");
+        else
+            System.out.println("NOT VALID!");
+    }
+}
---
+++ LuhnCheck/app/src/test/java/com/tbruej/luhnLocalTest/LuhnLocalTest.java
@@ -0,0 +1,29 @@
+package com.tbruej.luhnLocalTest;
+
+import com.tbruej.localLuhn.luhn;
+import org.junit.*;
+
+import static com.tbruej.localLuhn.luhn.checkLuhn;
+import static org.junit.Assert.*;
+
+
+public class LuhnLocalTest {
+
+
+
+    //@Test(expected = AssertionError.class)
+    @Test
+    public void luhnTest(){
+        //assertEquals(checkLuhn("18009054331492"), true);
+        assertEquals(checkLuhn("180090543314"), false);
+    }
+
+
+    @Test(expected = AssertionError.class)
+    public void luhnFailTest(){
+        //assertEquals(checkLuhn("18009054331492"), true);
+        assertEquals(checkLuhn("180090543314"), true);
+    }
+
+
+}
